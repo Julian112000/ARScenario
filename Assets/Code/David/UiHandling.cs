5824c29b675cf480b6e6c0e6633f8f53e323ea3e
David <david_W@live.nl>
2019-02-28 16:00:46 +0100

    Broken script fixed
    Er was 1 dikke error die is nu weg

---
+++ Assets/Code/David/UiHandling.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Models
+{
+    Tank,
+    Truck,
+    Jeep
+}
+public class UiHandling : MonoBehaviour {
+
+    public GameObject CurrentModel;
+    public Models models = Models.Tank;
+    [SerializeField]
+    private List<GameObject> ModelList;
+
+    private void Awake()
+    {
+        CurrentModel = ModelList[0];
+    }
+
+    public void SwitchModels()
+    {
+        models = models + 1;
+        CurrentModel = ModelList[(int)models];
+        if((int)models > ModelList.Count)
+        {
+            models = 0;
+            CurrentModel = ModelList[(int)models];
+        }
+    }
+}
