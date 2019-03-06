cc1ce575eb37eb4c293d008c6d0d2be5f781c777
David <david_W@live.nl>
2019-03-06 15:50:50 +0100

    Veel Updates
    Build System
    - rotating
    - placing
    - Scaling
    En dan ook nog EventSystem en statehandler
    - Buildmode
    - Pausemode
    - PlayMode
    
    GG wp

--- Assets/Code/David/ARController.cs
+++ Assets/Code/David/ARController.cs
@@ -1,88 +1,175 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GoogleARCore;
+﻿namespace GoogleARCore.Examples.HelloAR
+{
+    //
+#if UNITY_EDITOR
+    // NOTE:
+    // - InstantPreviewInput does not support `deltaPosition`.
+    // - InstantPreviewInput does not support input from
+    //   multiple simultaneous screen touches.
+    // - InstantPreviewInput might miss frames. A steady stream
+    //   of touch events across frames while holding your finger
+    //   on the screen is not guaranteed.
+    // - InstantPreviewInput does not generate Unity UI event system
+    //   events from device touches. Use mouse/keyboard in the editor
+    //   instead.
+    using Input = InstantPreviewInput;
+#endif
 
-public class ARController : MonoBehaviour {
-
-
-
-
-    private List<DetectedPlane> Trackedplanes = new List<DetectedPlane>();
-    [SerializeField]
-    private GameObject GridPrefab;
-    [SerializeField]
-    private UiHandling uihandling;
-    [SerializeField]
-    private Camera FirstpersonCam;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Session.Status != SessionStatus.Tracking)
-        {
-            Debug.Log("NOT TRACKING");
-            return;
-        }
-
-        Session.GetTrackables<DetectedPlane>(Trackedplanes, TrackableQueryFilter.New);
-
-        for (int i = 0; i < Trackedplanes.Count; i++)
-        {
-            GameObject Grid = Instantiate(GridPrefab.gameObject, Vector3.zero, Quaternion.identity, transform);
-
-            Grid.GetComponent<GridVisualiser>().Initialize(Trackedplanes[i]);
-        }
-        ScreenInput();
-
-
-    }
-
-    void ScreenInput()
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using GoogleARCore;
+    //
+    public enum ControllerState
     {
-        // If the player has not touched the screen, we are done with this update.
-        Touch touch;
-        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
-        {
-            return;
-        }
-
-        // Raycast against the location the player touched to search for planes.
-        TrackableHit hit;
-        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
-            TrackableHitFlags.FeaturePointWithSurfaceNormal;
-
-        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
-        {
-            // Use hit pose and camera pose to check if hittest is from the
-            // back of the plane, if it is, no need to create the anchor.
-            if ((hit.Trackable is DetectedPlane) &&
-                Vector3.Dot(FirstpersonCam.transform.position - hit.Pose.position,
-                    hit.Pose.rotation * Vector3.up) < 0)
-            {
-                Debug.Log("Hit at back of the current DetectedPlane");
-            }
-            else
-            {
-                GameObject prefab;
-                prefab = uihandling.CurrentModel;
-
-                GameObject SpawnedObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
-
-                // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
-                SpawnedObject.transform.Rotate(0, 180, 0, Space.Self);
-
-                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
-                // world evolves.
-                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-
-                // Make Andy model a child of the anchor.
-                SpawnedObject.transform.parent = anchor.transform;
-            }
-        }
+        Placing,
+        Scaling,
+        Rotating
     }
 
+
+
+    public class ARController : MonoBehaviour
+    {
+
+        //
+        [SerializeField]
+        private float ScaleSpeed = 0.05f;
+        [SerializeField]
+        private float RotateSpeed = 0.5f;
+        //
+        [SerializeField]
+        private Camera FirstpersonCam;
+        [SerializeField]
+        private GameObject Model;
+        public ControllerState controllerstate;
+        [SerializeField]
+        private GameObject CurrentplacedObject;
+        [SerializeField]
+        GameObject GridViewer;
+        //
+        Vector2 FirstPos;
+        Vector2 LastPos;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log(Session.Status);
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                Debug.Log("NOT TRACKING");
+                return;
+            }
+            if (controllerstate == ControllerState.Placing)
+            {
+                ScreenInputPlacing();
+                GridViewer.SetActive(true);
+            }
+            if (controllerstate == ControllerState.Scaling)
+            {
+                ScreenInputScaling();
+                GridViewer.SetActive(false);
+            }
+            if (controllerstate == ControllerState.Rotating)
+            {
+                ScreenInputRotating();
+                GridViewer.SetActive(false);
+            }
+
+        }
+
+        void ScreenInputPlacing()
+        {
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                return;
+            }
+
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+            {
+                if ((hit.Trackable is DetectedPlane) &&
+                    Vector3.Dot(FirstpersonCam.transform.position - hit.Pose.position,
+                        hit.Pose.rotation * Vector3.up) < 0)
+                {
+                    Debug.Log("Hit at back of the current DetectedPlane");
+                }
+                else
+                {
+
+                    GameObject SpawnedObject = Instantiate(Model, hit.Pose.position, hit.Pose.rotation);
+                    SpawnedObject.transform.Rotate(0, 180, 0, Space.Self);
+                    CurrentplacedObject = SpawnedObject;
+                    // world evolves.
+                    Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                    SpawnedObject.transform.parent = anchor.transform;
+                    //
+                }
+            }
+        }
+
+        void ScreenInputScaling()
+        {
+            if (Input.touchCount == 2)
+            {
+                // Store both touches.
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                // Find the position in the previous frame of each touch.
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                // Find the magnitude of the vector (the distance) between the touches in each frame.
+                float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+                // Find the difference in the distances between each frame.
+                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                //
+
+                CurrentplacedObject.transform.localScale += new Vector3(CurrentplacedObject.transform.localScale.x * deltaMagnitudeDiff * ScaleSpeed, CurrentplacedObject.transform.localScale.y * deltaMagnitudeDiff * ScaleSpeed, CurrentplacedObject.transform.localScale.z * deltaMagnitudeDiff * ScaleSpeed);
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                FirstpersonCam.fieldOfView = Mathf.Clamp(FirstpersonCam.fieldOfView, 0.1f, 179.9f);
+            }
+
+        }
+
+        void ScreenInputRotating()
+        {
+            if(Input.touchCount == 1)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Debug.Log("began");
+                    FirstPos = touch.position;
+                    LastPos = touch.position;
+                }
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    LastPos = touch.position;
+                    if (FirstPos.x < LastPos.x)
+                    {
+                        Debug.Log("Swiping right");
+                        CurrentplacedObject.transform.Rotate(new Vector3(0, RotateSpeed, 0));
+
+                    }
+                    else if (LastPos.x < FirstPos.x)
+                    {
+                        Debug.Log("Swiping Left");
+                        CurrentplacedObject.transform.Rotate(new Vector3(0, RotateSpeed, 0));
+
+                    }
+                }
+            }
+        }
+
+
+    }
 }
---
+++ Assets/SceneHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneState
+{
+    BuildMode,
+    Playing,
+    Paused
+}
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneState CurrentSceneState = SceneState.BuildMode;
+    //
+    public delegate void BuildMode();
+    public static event BuildMode SwitchToBuildMode;
+    //
+    public delegate void PlayMode();
+    public static event PlayMode SwitchToPlayMode;
+    //
+    public delegate void PauseMode();
+    public static event PauseMode SwitchToPauseMode;
+
+    public static void EnableBuildMode()
+    {
+        SwitchToBuildMode();
+        CurrentSceneState = SceneState.BuildMode;
+    }
+    public static void EnablePlayMode()
+    {
+        SwitchToPlayMode();
+        CurrentSceneState = SceneState.Playing;
+    }
+    public static void EnablePauseMode()
+    {
+        SwitchToPauseMode();
+        CurrentSceneState = SceneState.Paused;
+    }
+}
